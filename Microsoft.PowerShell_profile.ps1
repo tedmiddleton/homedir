1dac5dff3921982224e4f8f7e1a416dbd5d5ffe5
Ted Middleton <middleton.ted@gmail.com>
2023-10-25 10:02:05 -0700

    Adding a powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,24 @@
+Set-PSReadlineOption -EditMode vi
+Set-Alias -Name la -Value ls
+Set-Alias -Name less -Value "${env:ProgramFiles}\Git\usr\bin\less.exe"
+Set-Alias -Name which -Value Get-Command
+
+$j_timer = New-Object System.Diagnostics.Stopwatch
+
+Set-PSReadLineKeyHandler -Key d -ViMode Insert -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("d")
+    $j_timer.Restart()
+}
+
+Set-PSReadLineKeyHandler -Key f -ViMode Insert -ScriptBlock {
+    if (!$j_timer.IsRunning -or $j_timer.ElapsedMilliseconds -gt 1000) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("f")
+    } else {
+        [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
+        $line = $null
+        $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+        [Microsoft.PowerShell.PSConsoleReadLine]::Delete($cursor, 1)
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor-1)
+    }
+}
